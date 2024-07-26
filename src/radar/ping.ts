5f7b2bb845c8ad2529fdb1681bdd83552a590174
David <d.graham8@rgu.ac.uk>
2024-07-26 10:55:23 +0100

    relocate ping

--- src/radar/ping.ts
+++ src/radar/ping.ts
@@ -1,3 +1,4 @@
+import { ORIGIN } from '../constants'
 import { State } from '../state'
 
 export const renderPing = (ctx: CanvasRenderingContext2D) => {
@@ -10,5 +11,5 @@ export const renderPing = (ctx: CanvasRenderingContext2D) => {
 
 	ctx.globalAlpha = State.imageOpacity
 	State.imageOpacity -= 0.008
-	ctx.drawImage(State.image, 480, 260, 200, 200)
+	ctx.drawImage(State.image, ORIGIN - 100, ORIGIN - 100, 200, 200)
 }
